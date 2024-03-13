684168f8142a8dd548bf69e5b7c88deb10109b01
unity-team-developers <cesarmartins.pro@gmail.com>
2024-03-13 16:00:11 +0100

    fix: si-products

--- ForgeReact/src/components/Product/dados.js
+++ ForgeReact/src/components/Product/dados.js
@@ -3,7 +3,19 @@ const productData = [
     img: "https://media.discordapp.net/attachments/1075408036073582673/1217282610175152248/6c10288b36a22380e51c83351424fa7f.png?ex=660375c6&is=65f100c6&hm=582c091e9bf359ad81deffee6cf3d9bd93bcacb65cfaf60488fc9ebb6caa4b3f&=&format=webp&quality=lossless",
     price: "329",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "Lorem Ipsum is simply dummy text of the printing Lorem Ipsum is simply dummy",
+  },
+  {
+    img: "https://media.discordapp.net/attachments/1075408036073582673/1217282610175152248/6c10288b36a22380e51c83351424fa7f.png?ex=660375c6&is=65f100c6&hm=582c091e9bf359ad81deffee6cf3d9bd93bcacb65cfaf60488fc9ebb6caa4b3f&=&format=webp&quality=lossless",
+    price: "329",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing Lorem Ipsum is simply dummy",
+  },
+  {
+    img: "https://media.discordapp.net/attachments/1075408036073582673/1217282610175152248/6c10288b36a22380e51c83351424fa7f.png?ex=660375c6&is=65f100c6&hm=582c091e9bf359ad81deffee6cf3d9bd93bcacb65cfaf60488fc9ebb6caa4b3f&=&format=webp&quality=lossless",
+    price: "329",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing Lorem Ipsum is simply dummy",
   },
   {
     img: "https://media.discordapp.net/attachments/1075408036073582673/1217282610175152248/6c10288b36a22380e51c83351424fa7f.png?ex=660375c6&is=65f100c6&hm=582c091e9bf359ad81deffee6cf3d9bd93bcacb65cfaf60488fc9ebb6caa4b3f&=&format=webp&quality=lossless",
@@ -21,19 +33,7 @@ const productData = [
     img: "https://media.discordapp.net/attachments/1075408036073582673/1217282610175152248/6c10288b36a22380e51c83351424fa7f.png?ex=660375c6&is=65f100c6&hm=582c091e9bf359ad81deffee6cf3d9bd93bcacb65cfaf60488fc9ebb6caa4b3f&=&format=webp&quality=lossless",
     price: "329",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    img: "https://media.discordapp.net/attachments/1075408036073582673/1217282610175152248/6c10288b36a22380e51c83351424fa7f.png?ex=660375c6&is=65f100c6&hm=582c091e9bf359ad81deffee6cf3d9bd93bcacb65cfaf60488fc9ebb6caa4b3f&=&format=webp&quality=lossless",
-    price: "329",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    img: "https://media.discordapp.net/attachments/1075408036073582673/1217282610175152248/6c10288b36a22380e51c83351424fa7f.png?ex=660375c6&is=65f100c6&hm=582c091e9bf359ad81deffee6cf3d9bd93bcacb65cfaf60488fc9ebb6caa4b3f&=&format=webp&quality=lossless",
-    price: "329",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesett.",
   },
 ];
 
--- ForgeReact/src/components/Product/product.jsx
+++ ForgeReact/src/components/Product/product.jsx
@@ -1,30 +1,34 @@
 export default function ProductComponent({ props }) {
   return (
-    <div className="flex flex-col w-full justify-center items-center">
+    <div className="flex flex-col w-full justify-between items-center">
       <div className="flex flex-col md:w-[1200px] justify-center items-center border">
-        <div className="flex flex-wrap pt-4 md:w-[875px] justify-center gap-4">
+        <div className="flex flex-wrap pt-4 md:w-[875px] justify-center md:gap-4 gap-2">
           {props.map((product, index) => (
             <div
               key={index}
-              className="flex flex-col justify-center items-center rounded-lg shadow overflow-hidden border w-[280px]"
+              className="flex flex-col justify-between items-center rounded-2xl shadow-xl overflow-hidden border md:w-[280px] w-[45%]"
+              id="card"
             >
               <img
-                className=" max-w-[280px] min-w-[280px] max-md:max-w-[40px]"
+                className=" max-w-[280px] min-w-[280px] max-md:max-w-[40px] "
                 src={product.img}
               />
-              <div className="shadow min-w-full rounded border text-start">
-                <div className="text-[#113B85] font-bold text-center flex flex-col">
+              <div className="shadow min-w-full rounded border text-start gap-2 pt-3 flex flex-col">
+                <div className="text-[#113B85]  font-bold text-center flex flex-col justify-center items-center ">
                   <h1>
                     R$<span className="text-2xl">{product.price}</span>,90
-                    <img
-                      src="https://media.discordapp.net/attachments/1075408036073582673/1217284018416910486/image.png?ex=66037716&is=65f10216&hm=8559b31bd010ad72e3c969e79c80828c152e5e93a5ac7c0754be791ad489798b&=&format=webp&quality=lossless"
-                      alt=""
-                    />
+                    <div className="flex flex-col pt-1">
+                      <img
+                        className="w-[100px] h-[16px]"
+                        src="https://media.discordapp.net/attachments/1075408036073582673/1217284018416910486/image.png?ex=66037716&is=65f10216&hm=8559b31bd010ad72e3c969e79c80828c152e5e93a5ac7c0754be791ad489798b&=&format=webp&quality=lossless"
+                        alt=""
+                      />
+                    </div>
                   </h1>
                 </div>
-                <div className="text-[8pt] h-[100px]">
-                  <p>{product.description}</p>
-                </div>
+                <p className="text-ellipsis overflow-hidden line-clamp-3 h-[100%] text-center px-2 pb-1">
+                  {product.description}
+                </p>
               </div>
             </div>
           ))}
